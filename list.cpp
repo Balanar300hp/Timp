6c99260399bf6199831ee862b0dcef2ea19b36c6
Balanar300hp <kudashov.vlad@yandex.ru>
2016-04-13 20:47:59 +0300

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -1,64 +1,125 @@
-#include <fstream>
-#include <iostream>
+#include "stdafx.h"
 #include <string.h>
+#include <iostream>
+#include <fstream>
 using namespace std;
-bool _home = false; 
-bool _end = false; 
-int inner = 1; 
-void out(char str[256])
+/** \func struct List */
+struct List
 {
-	char outstr[256];
-	char outbuf[256];
-	int p = 256;
-	if (strstr(str, "/**") != NULL)
-	{
-		_home = true;
-		_end = false;
-	}
-	if ((_home == true) && (_end == false))
-	{
-		if (strstr(str, "\\func") != NULL) 
-		{
+	int value;
+	List *pNext;
+	List *pPrev;
+};
+List *head; 
 
-			for (int j = 0; j < 256; j++)
-			{
-				outbuf[j] = *(strstr(str, "func") + 5+j);
-			}
-			p = strcspn(outbuf, ")");
-			for (int j = p + 1; j < 256; j++)
-			{
-				outbuf[j] = 0;
-			}
-			cout << endl;
-			cout << inner << ") " << outbuf << endl;
-			inner++;
-			cout << endl;
-		}
-	}
-	if (strstr(str, "*/") != NULL)
+/** \func void addpos(int newvalue, int position) */
+void addpos(int newvalue, int position)
+{
+	List  *node = new List; 
+	node->value = newvalue;
+	if ((head == NULL))
 	{
-		_end = true;
+		node->pNext = node; 
+		node->pPrev = node;
+		head = node; /
+	}
+	else
+	{
+		List *p = head;
+		for (int i = position; i > 0; i--)
+		{
+			p = p->pNext;
+		}
+		p->pPrev->pNext = node;
+		node->pPrev = p->pPrev;
+		node->pNext = p; 
+		p->pPrev = node;
 	}
 }
 
+/** \func void Delete(int position) */
+void Delete(int position)
+{
+	if (head == NULL) 
+		{ 
+		cout << "\nÏóñòîé ñïèñîê\n"; 
+		}
+	if (head->pNext == head) 
+	{
+		delete head;
+		head = NULL;
+	}
+	else
+	{
+		List *_del = head;
+		for (int i = position; i>0; i--) _del = _del->pNext;
+		if (_del == head) head = _del->pNext;
+		_del->pPrev->pNext = _del->pNext;
+		_del->pNext->pPrev = _del->pPrev;
+		delete _del;
+	}
+}
 
+/** \func int Get(int position) */
+int Get(int position)
+{
+	int ch;
+	if (head == NULL) { cout << "\nÑïèñîê ïóñò\n\n"; return 0; }
+	else
+	{
+		List *_get = head;
+		for (int i = position; i > 0; i--) _get = _get->pNext;
+		ch = _get->value;
+	}
+	return ch;
+}
+
+/** \func int main() */
 /*int main()
 {
+	int numact, pos, val;
+	int k = 0;
+	int exit[20];
+	char str[64];
+	char *act = new char[64];
 	setlocale(LC_CTYPE, "rus");
-	char path[256];
-	char buffstr[256];
+	cout << "ñïèñîê :" << endl;
+	cout << "âõîä :" << endl;
 	fstream fin;
-	cin >> path;
-	fin.open(path, ios::in);
+	fin.open("text.txt", ios::in);
 	if (fin.is_open())
 	{
-		while (!fin.eof())
+		fin.getline(str, 64);
+		cout << str << endl;
+		numact = atoi(str);
+		for (int i = 0; i < numact; i++)
 		{
-			fin.getline(buffstr, 256);
-			output(buffstr);
+			fin.getline(str, 64);
+			cout << str << endl;
+			act = strtok(str, " ");
+			if (!strcmp(act, "add"))
+			{
+				pos = atoi(strtok(NULL, " "));
+				val = atoi(strtok(NULL, " "));
+				addpos(val, pos);
+			}
+			if (!strcmp(act, "del"))
+			{
+				pos = atoi(strtok(NULL, " "));
+				Delete(pos);
+			}
+			if (!strcmp(act, "get"))
+			{
+				pos = atoi(strtok(NULL, " "));
+				exit[k] = GetElement(pos);
+				k++;
+			}
 		}
 	}
-	system("pause");
 	fin.close();
+	cout << endl << "âûõîä :" << endl;
+	for (int i = 0; i < k; i++)
+		cout << exit[i] << endl;
+	system("pause");
 	return 0;
 }*/
